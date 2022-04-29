f703152815487c618c73f63067b88541bd1c8826
Luuk Moret <luuk@moretholding.com>
2022-04-29 11:05:44 +0200

    update site

--- svelte.config.js
+++ svelte.config.js
@@ -3,27 +3,27 @@ import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: [
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs'
-		}),
-		prerender: {
-			default: true
-		},
-		browser: {
-			hydrate: process.env.NODE_ENV === 'development',
-			router: false
-		}
-	}
+  kit: {
+    adapter: adapter({
+      pages: 'docs',
+      assets: 'docs',
+    }),
+    prerender: {
+      default: true,
+    },
+    browser: {
+      hydrate: process.env.NODE_ENV === 'development',
+      router: false,
+    },
+  },
 };
 
 export default config;
